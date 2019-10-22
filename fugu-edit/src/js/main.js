70ffe1ec36f0b5cdbc10c90e9e1b997980d29ff7
Sam Richard <snugug@google.com>
2019-10-22 08:06:14 -0400

    :shirt: Add launchQueue to globals

--- fugu-edit/src/js/main.js
+++ fugu-edit/src/js/main.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/* global CodeMirror */
+/* global CodeMirror, launchQueue */
 import { log } from './lib/log';
 import { selectFile, selectFolder, openFile, writeFile } from './lib/files';
 
